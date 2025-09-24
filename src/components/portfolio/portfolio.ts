6537896beba112291d5beda6434f9cc0e7b0dad4
TarikZaki <zakitarek20@gmail.com>
2025-09-24 22:20:16 +0300

    fixing imgs path

--- src/components/portfolio/portfolio.ts
+++ src/components/portfolio/portfolio.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
-import { Star } from "../star/star";
+import { Star } from '../star/star';
 
 @Component({
   selector: 'app-portfolio',
   imports: [Star],
   templateUrl: './portfolio.html',
-  styleUrl: './portfolio.css'
+  styleUrl: './portfolio.css',
 })
 export class Portfolio {
-  title: string = "Portfolio component"
-  lineClr: string = 'var(--main-clr)'
-  flag: boolean = true
-  modelImg!: string
+  title: string = 'Portfolio component';
+  lineClr: string = 'var(--main-clr)';
+  flag: boolean = true;
+  modelImg!: string;
   hideModel(element: EventTarget | null, img: HTMLImageElement) {
     if (element == img) return;
     this.flag = true;
   }
-  PortfolioSrc: string[] = ['/assets/port1.png', '/assets/port2.png', '/assets/port3.png', '/assets/port1.png', '/assets/port2.png', '/assets/port3.png']
+  PortfolioSrc: string[] = [
+    'assets/port1.png',
+    'assets/port2.png',
+    'assets/port3.png',
+    'assets/port1.png',
+    'assets/port2.png',
+    'assets/port3.png',
+  ];
 }
-
